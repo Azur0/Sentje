957e0b4f81dbd688040a9a671366f53a6260b893
Jeroen3529 <Jeroen3529@gmail.com>
2019-02-24 16:10:51 +0100

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,13 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-
+<div id="banner"></div>
 <div id="form_login">
-	<label>email</label><input type="email" name="email" required="true">
-	<label>password</label><input type="password" name="password" required="true">
-	<div>
-		<input type="button" name="login" value="login">
-	</div>
+	<form>
+		<label>email</label><input type="email" name="email" required="true">
+		<label>password</label><input type="password" name="password" required="true">
+		<div>
+			<input class="btn btn-primary btn-xl text-uppercase" type="button" name="login" value="login">
+		</div>
+	</form>
 </div>
 
 @endsection
