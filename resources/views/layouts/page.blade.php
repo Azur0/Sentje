a19c580117fdc768989e0cbd2170c832a00cbc2d
Jeroen3529 <Jeroen3529@gmail.com>
2019-02-24 15:30:58 +0100

    ;;

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" dir="ltr">
     <head>
-        @include('partials._metaData');
+        @include('partials._metaData')
 
         <title><?= $title . ' | ' ?>{{ config('app.name', 'Sentje') }}</title>
         <link rel="icon" type="image/png" href=".\img\icon.png" />
         
-        @include('partials._styles');
+        @include('partials._styles')
 
     </head>
     <body id="page-top">
