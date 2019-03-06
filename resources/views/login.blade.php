d3bd7055e3d7015274f93e7210e571d6b83b00e2
Jeroen3529 <Jeroen3529@gmail.com>
2019-03-06 19:52:37 +0100

    register_login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,9 +9,14 @@
 			<label for="email">Email</label><input type="email" name="email" required="true">
 			<label for="password">Password</label><input type="password" name="password" required="true">
 		</div>
+		<div id="form_response">
+			
+		</div>
+		<div id="logreg_other">
+			don't have an account? Register <a href="./login">here</a>
+		</div>
 		<div class="form_buttons">
 			<input class="btn btn-primary btn-xl text-uppercase" type="button" name="login" value="login">
-			<a href="./register" class="btn btn-primary btn-xl text-uppercase">register</a>
 		</div>
 	</form>
 </div>
