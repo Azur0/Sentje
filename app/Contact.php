993552de10d50da8abe93fce09571d49cdf39907
azur0 <bimre@avans.nl>
2019-03-15 19:35:46 +0100

    no message

--- app/Contact.php
+++ app/Contact.php
@@ -14,4 +14,8 @@ class Contact extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function user1() {
+        return $this->belongsTo(User::class, 'user_id1');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -15,6 +15,25 @@
                     @endif
 
                     You are logged in!
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Contacts</div>
+
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <tbody>
+                            @foreach ($friends as $friend)
+                                <tr>
+                                    <td>
+                                        <span style="vertical-align: middle;">{{ $friend->user_id1 . ' ' . $friend->user1->name }}</span><span class="float-right"><a href=""><i class="fas fa-plus-square" style="font-size: 30px; vertical-align: middle;"></i></a></span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
