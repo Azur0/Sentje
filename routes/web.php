687756a20e24367352bca5b1e73bf2cdef160f1e
azur0 <bimre@avans.nl>
2019-03-23 16:35:14 +0100

    added localization support for Dutch and English

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('locale/{locale?}', array('as' => 'set-locale', 'uses' => 'LanguageController@setLocale'));
 
 Route::resource('currency', 'CurrencyController');
 
