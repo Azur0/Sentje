9bf5b651d25ae84685ff62b261c11b18995fcab6
Jeroen3529 <Jeroen3529@gmail.com>
2019-03-31 15:26:34 +0200

    commit

--- app/Http/Controllers/PaymentRequestController.php
+++ app/Http/Controllers/PaymentRequestController.php
@@ -201,21 +201,18 @@ class PaymentRequestController extends Controller
 
 		if($paymentRequest->created_by_user_id == Auth::user()->id)
 		{
-			//$mollie->payments->delete("tr_WDqYK6vllg");
+			$mollie->payments->delete($paymentRequest->mollie_id);
 		}
 
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  \App\PaymentRequest  $paymentRequest
-	 * @return \Illuminate\Http\Response
-	 */
 	public function success($succesurl)
 	{
-
 		$paymentRequest = PaymentRequest::where('success_url', $succesurl)->first();
+		
+		$payment = $mollie->payments->get($paymentRequest->mollie_id);
+
+		dd($payment);
 
 		if($paymentRequest !== null)
 		{
